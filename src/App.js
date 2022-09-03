d179ce64a1532cb74669fb34decaf5d40ba4de1a
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-09-03 16:27:49 -0300

    propTypes validation on Form.jsx Component should be Number on cardAttr1, cardAttr2 n cardAttr3 props. Fixing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    console.log(this.state);
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
+      cardImage: '',
+      cardRare: ''
+      cardTrunfo: false,
+    };
+  }
+
   render() {
     return (
       <div>
