77d1b1c2406b2b968f1389035a5cf6a3a90d02d8
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-09-05 05:27:30 -0300

    Almost 3 hours cracking my mind. Forgot to add this.state SPREAD operator on Form Component in App.js

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: 0,
-      cardAttr2: 0,
-      cardAttr3: 0,
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
       cardImage: '',
       cardRare: '',
       cardTrunfo: false,
@@ -24,40 +24,35 @@ class App extends React.Component {
   onInputChange(event) {
     const userInputValue = event.target.type === 'checkbox'
       ? event.target.checked : event.target.value;
-
     this.setState({
       [event.target.name]: userInputValue,
-    }, this.enablingSaveButton());
+    }, () => {
+      const { cardName, cardDescription, cardAttr1,
+        cardAttr2, cardAttr3, cardImage } = this.state;
+      const MAX = 210;
+      const MIN = 90;
+      if (cardName !== ''
+      && cardImage !== ''
+      && cardDescription !== ''
+      && Number(+cardAttr1) <= MIN
+      && Number(+cardAttr2) <= MIN
+      && Number(+cardAttr3) <= MIN
+      && Number(+cardAttr1) >= 0
+      && Number(+cardAttr2) >= 0
+      && Number(+cardAttr3) >= 0
+      && (Number(+cardAttr1) + Number(+cardAttr2) + Number(+cardAttr3)) <= MAX) {
+        this.setState({ isSaveButtonDisabled: false });
+      } else {
+        this.setState({ isSaveButtonDisabled: true });
+      }
+    });
   }
-
-  enablingSaveButton = () => {
-    const { cardName, cardDescription, cardAttr1,
-      cardAttr2, cardAttr3, cardImage } = this.state;
-   /*  const MAX = 210;
-    const MIN = 90;
-    if (cardName.length > 0
-    && cardImage.length > 0
-    && cardDescription.length > 0
-    && cardAttr1 <= MIN
-    && cardAttr2 <= MIN
-    && cardAttr3 <= MIN
-    && cardAttr1 > 0
-    && cardAttr2 > 0
-    && cardAttr3 > 0
-    && ((cardAttr1 + cardAttr2 + cardAttr3) < MAX)) */
-    console.log(cardName);
-    if (cardName.length > 5) {
-      this.setState({ isSaveButtonDisabled: false });
-    } else {
-      this.setState({ isSaveButtonDisabled: true });
-    }
-  };
 
   render() {
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form onInputChange={ this.onInputChange } />
+        <Form { ...this.state } onInputChange={ this.onInputChange } />
         <Card { ...this.state } />
       </div>
     );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -69,7 +69,7 @@ class Form extends Component {
           <input
             type="text"
             data-testid="image-input"
-            name="imageURL"
+            name="cardImage"
             value={ cardImage }
             onChange={ onInputChange }
           />
@@ -114,9 +114,9 @@ class Form extends Component {
 Form.propTypes = {
   cardName: propTypes.string.isRequired,
   cardDescription: propTypes.string.isRequired,
-  cardAttr1: propTypes.number.isRequired,
-  cardAttr2: propTypes.number.isRequired,
-  cardAttr3: propTypes.number.isRequired,
+  cardAttr1: propTypes.string.isRequired,
+  cardAttr2: propTypes.string.isRequired,
+  cardAttr3: propTypes.string.isRequired,
   cardImage: propTypes.string.isRequired,
   cardRare: propTypes.string.isRequired,
   cardTrunfo: propTypes.bool.isRequired,
